3e6bc16dd2095d04f8866ccfad38624446e37a31
Ryan Lee <drdgvhbh@gmail.com>
2019-10-31 14:05:17 -0400

    Add Doc comments to the file system

--- pkg/fs.go
+++ pkg/fs.go
@@ -4,16 +4,19 @@ import (
 	"bazil.org/fuse/fs"
 )
 
+// FS -- Filesystem
 type FS struct {
 	root Dir
 }
 
+// NewFs -- Create a new filesystem
 func NewFs() *FS {
 	return &FS{
 		root: Dir{},
 	}
 }
 
+// Root - The root of the file system
 func (f FS) Root() (fs.Node, error) {
 	return f.root, nil
 }
