d6c052ae5a0d6e1a33e9999652ac7065dd2f279b
Ryan Lee <drdgvhbh@gmail.com>
2019-10-31 18:39:24 -0400

    Add Tests for listing directories

--- pkg/dir_test.go
+++ pkg/dir_test.go
@@ -37,3 +37,49 @@ func TestDirectoryHasInitialBlockSizeofDefaultSize(t *testing.T) {
 		pkg.DefaultSize,
 		fmt.Sprintf("The directory should have size of %d", pkg.DefaultSize))
 }
+
+func TestDirectoryShouldBeAbleToListItself(t *testing.T) {
+	dirs, err := pkg.NewDir(0, MockINodeGenerator{}).ReadDirAll(context.TODO())
+	assert.NoError(t, err)
+
+	var selfDir *fuse.Dirent
+	for _, dir := range dirs {
+		if dir.Name == "." {
+			selfDir = &dir
+		}
+	}
+
+	assert.NotNilf(t, selfDir, "the directory should be able to list itself")
+}
+
+func TestDirectoryShouldBeAbleToListItsParent(t *testing.T) {
+	dirs, err := pkg.NewDir(0, MockINodeGenerator{}).ReadDirAll(context.TODO())
+	assert.NoError(t, err)
+
+	var parentDir *fuse.Dirent
+	for _, dir := range dirs {
+		if dir.Name == ".." {
+			parentDir = &dir
+		}
+	}
+
+	assert.NotNilf(t, parentDir, "the directory should be able to list its parent")
+}
+
+func TestRootDirectoryShouldHaveItselfAsParent(t *testing.T) {
+	dirs, err := pkg.NewDir(0, MockINodeGenerator{}).ReadDirAll(context.TODO())
+	assert.NoError(t, err)
+
+	var selfDir *fuse.Dirent
+	var parentDir *fuse.Dirent
+	for _, dir := range dirs {
+		if dir.Name == "." {
+			selfDir = &dir
+		}
+		if dir.Name == ".." {
+			parentDir = &dir
+		}
+	}
+
+	assert.Equalf(t, selfDir, parentDir, "if the directory is root, its parent should be itself")
+}
